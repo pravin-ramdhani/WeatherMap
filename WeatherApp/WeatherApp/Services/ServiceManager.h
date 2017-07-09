feae6efcfd7c1ea93db12efc4809501caef437c9
Swapnali <swapnali.shinde@e-zest.in>
2017-07-09 16:15:45 +0530

    network call added

---
+++ WeatherApp/WeatherApp/Services/ServiceManager.h
@@ -0,0 +1,13 @@
+//
+//  ServiceManager.h
+//  WeatherApp
+//
+//  Created by Swapnali on 09/07/17.
+//  Copyright © 2017 WeatherMap. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ServiceManager : NSObject
+
+@end
