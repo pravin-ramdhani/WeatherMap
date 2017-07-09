30425e3ee43de64871f3cdb507380ca30b62886d
Swapnali <swapnali.shinde@e-zest.in>
2017-07-10 00:16:01 +0530

    forcast view added

---
+++ WeatherApp/WeatherApp/CustomViews/MoreCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MoreCollectionViewCell.h
+//  WeatherApp
+//
+//  Created by Swapnali on 09/07/17.
+//  Copyright © 2017 WeatherMap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoreCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ WeatherApp/WeatherApp/CustomViews/MoreCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  MoreCollectionViewCell.m
+//  WeatherApp
+//
+//  Created by Swapnali on 09/07/17.
+//  Copyright © 2017 WeatherMap. All rights reserved.
+//
+
+#import "MoreCollectionViewCell.h"
+
+@implementation MoreCollectionViewCell
+
+@end
