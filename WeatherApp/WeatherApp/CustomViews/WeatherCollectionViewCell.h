9be52821cefd5e1b9be2fccb7b1637488e409b12
Swapnali <swapnali.shinde@e-zest.in>
2017-07-09 21:03:23 +0530

    get weather report from api provided

---
+++ WeatherApp/WeatherApp/CustomViews/WeatherCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  WeatherCollectionViewCell.h
+//  WeatherApp
+//
+//  Created by Swapnali on 09/07/17.
+//  Copyright © 2017 WeatherMap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WeatherCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ WeatherApp/WeatherApp/CustomViews/WeatherCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  WeatherCollectionViewCell.m
+//  WeatherApp
+//
+//  Created by Swapnali on 09/07/17.
+//  Copyright © 2017 WeatherMap. All rights reserved.
+//
+
+#import "WeatherCollectionViewCell.h"
+
+@implementation WeatherCollectionViewCell
+
+@end
