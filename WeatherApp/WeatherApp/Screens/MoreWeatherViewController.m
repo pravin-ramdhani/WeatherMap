33e804f667bdbf5a90eeb697b3fa4e3ccabdab83
Swapnali <swapnali.shinde@e-zest.in>
2017-07-09 22:48:15 +0530

    cityview modifications

---
+++ WeatherApp/WeatherApp/Screens/MoreWeatherViewController.m
@@ -0,0 +1,37 @@
+//
+//  MoreWeatherViewController.m
+//  WeatherApp
+//
+//  Created by Swapnali on 09/07/17.
+//  Copyright © 2017 WeatherMap. All rights reserved.
+//
+
+#import "MoreWeatherViewController.h"
+
+@interface MoreWeatherViewController ()
+
+@end
+
+@implementation MoreWeatherViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
